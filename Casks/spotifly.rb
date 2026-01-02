453615405cc619c34238e8ea349d10759505a7a8
Ralph von der Heyden <ralph@rvdh.de>
2026-01-02 15:20:05 +0100

    Update Spotifly to version 1.0.1

--- Casks/spotifly.rb
+++ Casks/spotifly.rb
@@ -1,6 +1,6 @@
 cask "spotifly" do
   version "1.0.1"
-  sha256 "c79dfb9a6abbba74cb83b4699eaf314693ad93ffc7729c29cd7032dfa85d9f98"
+  sha256 "0cd6987b76ccadfdc1b3fb9159f2240cbb51ba454f55e4cb89f3ae78130cb6c8"
 
   url "https://github.com/ralph/homebrew-spotifly/releases/download/v#{version}/Spotifly-#{version}.zip"
   name "Spotifly"
