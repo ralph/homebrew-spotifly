12ccfc333291cf825ed56ac8031a2f391fcb3131
Ralph von der Heyden <ralph@rvdh.de>
2026-02-27 22:04:58 +0100

    Update homewbrew to 1.2.3

--- Casks/spotifly.rb
+++ Casks/spotifly.rb
@@ -1,8 +1,8 @@
 cask "spotifly" do
-  version "1.2.2"
-  sha256 "db02adba1a91bda1406dadae53f38e373b1dd90b3265a0af2307dddb3bd969dd"
+  version "1.2.3"
+  sha256 "75c9e62f9bc0679858b11de7712c7fb493c31c4f4a64172c9f7f254d59645ae7"
 
-  url "https://github.com/ralph/homebrew-spotifly/releases/download/v#{version}/Spotifly-#{version}.zip"
+  url "https://github.com/ralph/Spotifly/releases/download/v#{version}/Spotifly-#{version}.zip"
   name "Spotifly"
   desc "Lightweight Spotify player for macOS"
   homepage "https://github.com/ralph/homebrew-spotifly"
